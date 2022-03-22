96731b762548951650a135390061ff0c8ee69930
elian gonzalez <elianngonzalez7@gmail.com>
2022-03-22 02:30:50 -0300

    migrations & coresetup

--- commons/conection.go
+++ commons/conection.go
@@ -1,15 +1,27 @@
 package commons
 
 import (
+	"API-REST/models"
 	"log"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func GetConection() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:root@test?charset=utf8")
+	dns := "root:@/test?parseTime=true"
+	db, err := gorm.Open("mysql", dns)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return db
+}
+
+func Migrate() {
+	db := GetConection()
+	defer db.Close()
+
+	log.Println("Migrando...")
+
+	db.AutoMigrate(&models.Persona{})
 }
--- commons/handler.go
+++ commons/handler.go
@@ -2,16 +2,43 @@ package commons
 
 import (
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func SendResponse(w http.ResponseWriter, status int, data []byte) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.WriteHeader(status)
 	w.Write(data)
 }
 
-func SendError(w http.ResponseWriter, status int, err error) {
+func SendError(w http.ResponseWriter, status int) {
+	data := []byte(`{"error":"error"}`)
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.WriteHeader(status)
-	w.Write([]byte(err.Error()))
+	w.Write(data)
+}
+
+func setCorsSetUp(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, req *http.Request) {
+			// Just put some headers to allow CORS...
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			// and call next handler!
+			next.ServeHTTP(w, req)
+		})
+}
+
+func EnableCORS(router *mux.Router) {
+	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+	}).Methods(http.MethodOptions)
+	router.Use(setCorsSetUp)
 }
